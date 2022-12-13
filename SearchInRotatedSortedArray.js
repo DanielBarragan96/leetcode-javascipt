69939c0c15dc6e5a5163719cf968e30aa4472035
DanielBarragan96 <ie702554@iteso.mx>
2022-12-12 19:52:24 -0600

    solution using for loop

--- SearchInRotatedSortedArray.js
+++ SearchInRotatedSortedArray.js
@@ -3,4 +3,15 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {};
+var search = function (nums, target) {
+  for (index = 0; index < nums.length; index++) {
+    if (nums[index] == target) {
+      return index;
+    }
+  }
+  return -1;
+};
+
+nums = [4, 5, 6, 7, 0, 1, 2];
+target = 0;
+console.table(search(nums, target));
