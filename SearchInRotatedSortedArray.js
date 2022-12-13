bf75107e118b9d2f34eb8022a65a160e164e5ee3
DanielBarragan96 <ie702554@iteso.mx>
2022-12-12 22:51:01 -0600

    solution using bubble sort

--- SearchInRotatedSortedArray.js
+++ SearchInRotatedSortedArray.js
@@ -4,12 +4,20 @@
  * @return {number}
  */
 var search = function (nums, target) {
-  for (index = 0; index < nums.length; index++) {
-    if (nums[index] == target) {
-      return index;
-    }
+  start = 0;
+  end = nums.length - 1;
+  while (start < end) {
+    mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] > nums[start]) {
+      if (nums[start] <= target && nums[mid] > target) end = mid - 1;
+      else start = mid + 1;
+    } else if (nums[mid] < nums[end]) {
+      if (nums[mid] < target && nums[end] >= target) start = mid + 1;
+      else end = mid - 1;
+    } else break;
   }
-  return -1;
+  return nums[end] === target ? end : -1;
 };
 
 nums = [4, 5, 6, 7, 0, 1, 2];
