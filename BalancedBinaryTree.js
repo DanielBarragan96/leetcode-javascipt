e2b2748b513e0fb925a2597a69f4faa9d23cdb67
DanielBarragan96 <ie702554@iteso.mx>
2023-09-03 20:52:17 -0600

    solution

--- BalancedBinaryTree.js
+++ BalancedBinaryTree.js
@@ -22,4 +22,46 @@ The number of nodes in the tree is in the range [0, 5000].
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isBalanced = function (root) {};
+var Height = function (root) {
+  if (root === null) return 0;
+  let leftHeight = Height(root.left);
+  let rightHeight = Height(root.right);
+
+  if (leftHeight < 0 || rightHeight < 0) return -1;
+  if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+  return Math.max(leftHeight, rightHeight) + 1;
+};
+
+var isBalanced = function (root) {
+  if (root === null) return true;
+  return Height(root) !== -1;
+};
+
+let node5 = {
+  val: 7,
+  left: null,
+  right: null,
+};
+let node4 = {
+  val: 15,
+  left: null,
+  right: null,
+};
+let node3 = {
+  val: 20,
+  left: node4,
+  right: node5,
+};
+let node2 = {
+  val: 9,
+  left: null,
+  right: null,
+};
+let node1 = {
+  val: 1,
+  left: node2,
+  right: node3,
+};
+
+console.log(isBalanced(node1));
