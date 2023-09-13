b329010904cbe96376628bb5d61b04daf66b5fa3
DanielBarragan96 <ie702554@iteso.mx>
2023-09-12 21:40:14 -0600

    implementation

--- ImplementTrie(PrefixTree).js
+++ ImplementTrie(PrefixTree).js
@@ -28,3 +28,49 @@ Constraints:
 word and prefix consist only of lowercase English letters.
 At most 3 * 104 calls in total will be made to insert, search, and startsWith.
 */
+
+var Trie = function () {
+  this.words = [];
+};
+
+/**
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function (word) {
+  this.words.push(word);
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function (wordSearch) {
+  for (let word of this.words) {
+    if (word === wordSearch) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/**
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function (prefix) {
+  for (let word of this.words) {
+    if (word.startsWith(prefix)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
