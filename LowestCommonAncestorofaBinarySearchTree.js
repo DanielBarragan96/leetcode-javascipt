b0160032221ca5b8340cac6bdfd3c1b254db2b6f
DanielBarragan96 <ie702554@iteso.mx>
2023-09-14 19:37:41 -0600

    function

--- LowestCommonAncestorofaBinarySearchTree.js
+++ LowestCommonAncestorofaBinarySearchTree.js
@@ -24,3 +24,19 @@ All Node.val are unique.
 p != q
 p and q will exist in the BST.
 */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {};
