9e88b8fe74e6a921113f0378a54a634bb0eda359
DanielBarragan96 <ie702554@iteso.mx>
2023-10-08 21:23:30 -0600

    solution

--- BinarySearch.js
+++ BinarySearch.js
@@ -23,4 +23,19 @@ nums is sorted in ascending order.
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {};
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (nums[middle] === target) return middle;
+    else if (nums[middle] < target) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search([9], 9));
