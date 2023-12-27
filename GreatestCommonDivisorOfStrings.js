edd41c1ae700ae5fca3cfe00b4d198d522d1627e
Daniel Barragán <ie702554@iteso.mx>
2023-12-27 10:47:02 -0600

    solution using recursion

--- GreatestCommonDivisorOfStrings.js
+++ GreatestCommonDivisorOfStrings.js
@@ -24,6 +24,15 @@ str1 and str2 consist of English uppercase letters.
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
+    if(str1 + str2 != str2 + str1){
+      return "";
+    } else if(str1 == str2){
+        return str1;
+    } else if(str1.length > str2.length){
+        return gcdOfStrings(str1.substring(str2.length), str2);
+    } else{
+        return gcdOfStrings(str2.substring(str1.length), str1)
+    }  
 };
 
 console.log(gcdOfStrings("ABCABC", "ABC"));
